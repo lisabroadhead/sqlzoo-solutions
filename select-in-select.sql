0d60c34c3bf872e1e08c578f26dfa18f8f59753e
Lisa Broadhead <37422388+lisabroadhead@users.noreply.github.com>
2022-02-25 15:41:36 -0600

    Current answer comes back with an error
    The answer as is comes back with an error:
    
    'You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '%) FROM world WHERE continent = 'Europe'' at line 1'

--- select-in-select.sql
+++ select-in-select.sql
@@ -48,8 +48,9 @@ Germany (population 80 million) has the largest population of the countries in E
 
 Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
 */
-SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany'), 0), %)
-FROM world
+SELECT name,CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany')* 100),'%')
+AS percentage
+FROM world 
 WHERE continent = 'Europe'
 
 --#6
